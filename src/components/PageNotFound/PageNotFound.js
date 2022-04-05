0dccddf0408f4329d0d1f9cb6776f3749dc450df
unknown <elena.crivoscheina@yandex.ru>
2022-04-05 18:50:35 +0300

    внести правки

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './PageNotFound.css';
 
 function PageNotFound() {
     const navigate = useNavigate();
 
-    function handleGoBack() {
-        navigate.go();
-    }
+    // function handleGoBack() {
+    //     navigate.go();
+    // }
 
     return (
       <section className="content not-found">
             <h2 className="not-found__title">404</h2>
             <p className="not-found__subtitle">Страница не найдена</p>
-            <Link
-                onClick={handleGoBack}
-                to={"/movies"}
+            <button
+                onClick={() => navigate(-1)}
                 className="not-found__text-link"
             >
                 Назад
-            </Link>
+            </button>
       </section>  
     );
 }
